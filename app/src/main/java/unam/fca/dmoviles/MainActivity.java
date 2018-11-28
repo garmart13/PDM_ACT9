1504e66f420b4009cea034543e722eff07c505bc
garmart13 <nayelly.garzon@gmail.com>
2018-11-27 18:43:06 -0600

    Calculadora de porcentajes

--- app/src/main/java/unam/fca/dmoviles/MainActivity.java
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -2,36 +2,40 @@ package unam.fca.dmoviles;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText,editText2;
+    TextView textViewResult;
+    Button button;
+    String n1, n2;
+    float  a=0, b=0;
 
     private final static int IMAGE_WIDTH = 400;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //Actividad 8 Creacion de una calculadora de porcentajes
-        //En esta actividad crearas una aplicacion que calcule la diferencia relativa en porcentaje entre dos numeros.
-
-
-        // El archivo del layout de la actividad se encuentra en la carpeta
-        // res/layout/activity_main.xml
         setContentView(R.layout.activity_main);
 
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        button = (Button) findViewById(R.id.button);
+        textViewResult = (TextView) findViewById(R.id.textViewResult);
 
-
-        //Actividades
-        //a) Ejecuta la aplicacion y familiarizate con el layout definido en R.layout.activity_main
-        //b) Crea la logica para calcular la diferencia relativa en porcentaje entre dos numeros
-        //c) La formula para calcular la diferencia relativa en porcentaje esta en: https://www.calculatorsoup.com/calculators/algebra/percent-change-calculator.php
-        //d) La aplicacion debe calcular la diferencia relativa en porcentaje de los valores contenidos en "editText" y "editText2"
-        //e) Al hacer click en el boton "button", la aplicacion debera calcular dicha diferencia y poner el resultado en el campo "textViewResult"
-        //f) La aplicacion debera manejar adecuadamente el cambio de rotacion para el resultado y los campos de texto
-        //e) Anota tus respuestas y las capturas de pantalla en un documento en Word
-        //f) Sube tu codigo al repositorio.
-        //g) Sube el documento Word a la plataforma Moodle. Incluye la liga a tu repositorio
-
-
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1=editText.getText().toString();
+                n2=editText2.getText().toString();
+                a=Float.parseFloat(n1);
+                b=Float.parseFloat(n2);
+                textViewResult.setText(+(b-a)/a*100+ "% de incremento");
+            }
+        });
     }
 }
